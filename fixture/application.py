6c855009679221126b58ae2e3f9557f6299fac46
Pav4l <pavel.gorev@gmail.com>
2020-12-27 21:35:28 +0300

    Удалённое управление по протоколу Telnet

--- fixture/application.py
+++ fixture/application.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -15,8 +16,10 @@ class Application:
             raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(7)
         self.session = SessionHelper(self)
-        self.base_url = base_url
         self.project = ProjectHelper(self)
+        self.james = JamesHelper(self)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
         try:
